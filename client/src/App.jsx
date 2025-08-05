8ea5f8f1a1c196f00ea8f32c8de61332189a86ed
mkprasadp <manikantaprasad04@example.com>
2025-08-05 11:05:14 +0530

    Java Full Stack

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from 'react'
+
+const App = () => {
+  const [message,setmessage] = useState();
+
+  useEffect(()=>{
+    fetch('http://localhost:8080').then(response=>response.text())
+    .then(data=>setmessage(data))
+  },[])
+
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px', color: 'red' }}>
+      <h1>{message || 'Loading...'}</h1>
+    </div>
+  )
+}
+
+export default App
