e6a167b7ac9371e374a76690ce2f74279d8fb9a0
sparkclub-vtu <s.p.a.r.kclub@gmail.com>
2025-08-26 13:14:08 +0530

    Java Full Stack

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ const App = () => {
   const [message,setmessage] = useState();
 
   useEffect(()=>{
-    fetch('http://localhost:8080' || 'https://javafull-production.up.railway.app').then(response=>response.text())
+    fetch('http://localhost:8080' || `${process.env.JAVA_BACKEND_URL}`).then(response=>response.text())
     .then(data=>setmessage(data))
   },[])
 
