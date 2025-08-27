9b57d991cf988990889d1e6a0ab65e9fe3d7bbe9
Manikanta Prasad Padala <167625652+mkprasadp@users.noreply.github.com>
2025-08-27 15:14:26 +0530

    Java Full Stack

--- server/src/main/java/com/example/server/Controller/UserController.java
+++ server/src/main/java/com/example/server/Controller/UserController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.*;
  
 
 @RestController
-@CrossOrigin(origins = {"http://localhost:5173", "https://java-full.vercel.app"})
+@CrossOrigin(origins = {"https://java-full.vercel.app"})
 public class UserController {
 	
     @GetMapping("/")
